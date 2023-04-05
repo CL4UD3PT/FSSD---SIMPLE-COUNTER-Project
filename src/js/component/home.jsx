6703f8e35fe8152ac4a052ddc1504d1d5ad47841
CL4UD3PT <cifpinho@gmail.com>
2023-04-05 21:24:01 +0000

    style: added dark and gradient to row of Digits container; paddings and margin

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,7 +5,7 @@ import { Digit } from "./digit";
 const Home = () => {
   return (
     <div className="container">
-      <div className="row text-center d-flex">
+      <div className="row text-center d-flex bg-dark bg-gradient py-3 px-4 rounded-2 mt-3">
         <Digit text="0" />
         <Digit text="0" />
         <Digit text="0" />
