bd1d473abc6c6935b51405f5b73e95e13e1372d3
CL4UD3PT <cifpinho@gmail.com>
2023-04-05 21:12:44 +0000

    mod: changed structure for Digits to container > row

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { Digit } from "./digit"
+import { Digit } from "./digit";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="d-flex p-2 text-center">
-			<Digit text="0"/>
-			<Digit text="0"/>
-			<Digit text="0"/>
-			<Digit text="0"/>
-			<Digit text="0"/>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="row text-center d-flex">
+        <Digit text="0" />
+        <Digit text="0" />
+        <Digit text="0" />
+        <Digit text="0" />
+        <Digit text="0" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
