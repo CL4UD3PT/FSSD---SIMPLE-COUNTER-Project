182df42213444a347e70bd95dd6b6350acbbdd4e
CL4UD3PT <cifpinho@gmail.com>
2023-04-05 21:13:36 +0000

    fix: added col class to Digit h1

--- src/js/component/digit.js
+++ src/js/component/digit.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Digit = (props) => {
     return (
-        <h1 className="px-3 py-4 m-1 text-bg-dark rounded-3">{props.text}</h1>
+        <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{props.text}</h1>
     );
 };
