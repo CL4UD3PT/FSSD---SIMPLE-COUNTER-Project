338e21689267a8cd1f78a32ac18f3c62ffacd4d1
CL4UD3PT <cifpinho@gmail.com>
2023-04-06 23:53:04 +0000

    update: counter is counting but with fixed number of digits

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-export const Counter = (numOfDigits) => {
+export const Counter = (props) => {
     return (
-        <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3"><i class="fa-regular fa-clock"></i></h1>
+        <>
+            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3"><i class="fa-regular fa-clock"></i></h1>
+            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / 10000) % 10}</h1>
+            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / 1000) % 10}</h1>
+            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / 100) % 10}</h1>
+            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / 10) % 10}</h1>
+            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / 1) % 10}</h1>
+        </>
     );
 }
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Digit } from "./digit";
 import { Counter } from "./counter";
 
 //create your first component
 const Home = () => {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  }, []);
+
   return (
     <div className="container">
       <div className="row text-center d-flex bg-dark bg-gradient py-3 px-4 rounded-2 mt-3">
-        {/* <Digit text="0" />
-        <Digit text="0" />
-        <Digit text="0" />
-        <Digit text="0" />
-        <Digit text="0" />
-        <Digit text="0" />
-        <Digit text="0" /> */}
-        <Counter />
+        <Counter time={timer} />
       </div>
     </div>
   );
