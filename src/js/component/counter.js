ec27e6705de9315c487e8533c4935f69e48303b2
CL4UD3PT <cifpinho@gmail.com>
2023-04-06 17:11:54 +0000

    component: Counter component created

---
+++ src/js/component/counter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Counter = (numOfDigits) => {
+    return (
+        <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3"><i class="fa-regular fa-clock"></i></h1>
+    );
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Digit } from "./digit";
+import { Counter } from "./counter";
 
 //create your first component
 const Home = () => {
   return (
     <div className="container">
       <div className="row text-center d-flex bg-dark bg-gradient py-3 px-4 rounded-2 mt-3">
+        {/* <Digit text="0" />
         <Digit text="0" />
         <Digit text="0" />
         <Digit text="0" />
         <Digit text="0" />
         <Digit text="0" />
-        <Digit text="0" />
-        <Digit text="0" />
+        <Digit text="0" /> */}
+        <Counter />
       </div>
     </div>
   );
