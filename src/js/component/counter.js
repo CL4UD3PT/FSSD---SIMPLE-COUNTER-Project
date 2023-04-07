fb38689ccd8a7b1c17d8bf220445cffc38f30995
CL4UD3PT <cifpinho@gmail.com>
2023-04-07 00:45:57 +0000

    update: now can select how much digits I want in counter when calling thee component

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 export const Counter = (props) => {
+
+    const digits = [];
+    for (let i = 0; i < props.numberOfDigits; i++){
+        if(i == 0) digits.unshift('1');
+        else digits.unshift(digits[i-1]+0);
+    }
+
     return (
         <>
-            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3"><i class="fa-regular fa-clock"></i></h1>
-            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / 10000) % 10}</h1>
-            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / 1000) % 10}</h1>
-            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / 100) % 10}</h1>
-            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / 10) % 10}</h1>
-            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / 1) % 10}</h1>
+            <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3"><i className="fa-regular fa-clock"></i></h1>
+            {digits.map((digit, i) => <h1 className="col px-3 py-4 m-1 text-bg-dark rounded-3">{Math.floor(props.time / Number(digit)) % 10}</h1>)}
         </>
     );
 }
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
   return (
     <div className="container">
       <div className="row text-center d-flex bg-dark bg-gradient py-3 px-4 rounded-2 mt-3">
-        <Counter time={timer} />
+        <Counter time={timer} numberOfDigits={6} />
       </div>
     </div>
   );
