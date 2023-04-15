15715a156df2ac0ffe52543aad03bae529727d9f
CL4UD3PT <cifpinho@gmail.com>
2023-04-15 21:52:35 +0000

    fixes: buttons play and stop fixed with countdown at 0; alert displays on countdown to 0

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,6 +11,8 @@ const Home = () => {
 
   // refresh counter
   useEffect(() => {
+    if(countdown && reachedZero)console.log('condição 2')
+
     if (isRunning) {
       // when countdown reaches 0
       if (countdown && timer === 0){
@@ -18,11 +20,12 @@ const Home = () => {
         setIsRunning(false);
       }
 
-      // increment/decrement timer
       const intervalFunc = () => {
+        // increment timer
         if (!countdown) {
           setTimer((timer) => timer + 1);
         }
+        // decrement timer
         if (countdown && timer > 0) {
           setTimer((timer) => timer - 1);
         }
@@ -51,9 +54,10 @@ const Home = () => {
       <hr></hr>
       <div className="row my-2 justify-content-center">
         <div className="col-2 d-flex justify-content-between">
-          { <i className={`fa-solid w-10 fs-2 m-3 cursor-pointer ${isRunning ? "fa-pause fa-bounce text-primary" : !isRunning && timer > 0 ? "fa-play fa-fade text-primary" : "fa-play text-primary"}`} onClick={()=>setIsRunning(!isRunning)}></i>}
-          {/* { <i className={`fa-solid w-10 fs-2 m-3 cursor-pointer ${start ? "fa-pause fa-bounce text-success" : "fa-play fa-fade text-primary"}`} onClick={()=>setIsRunning(!start)}></i>} */}
-          { <i className={`fa-solid w-10 fa-stop fs-2 m-3 ${isRunning || timer > 0 || countdown ? "text-primary cursor-pointer" : "text-secondary"}`} onClick={()=>{reset()}}></i>}
+          { <i className={`fa-solid w-10 fs-2 m-3 cursor-pointer ${isRunning ? "fa-pause fa-bounce text-primary" : !isRunning && timer > 0 ? "fa-play fa-fade text-primary" : (countdown && reachedZero) ? "fa-play text-secondary" : "fa-play text-primary"}`} onClick={ () => {
+              if(!isRunning && countdown && timer===0){console.log('Please... insert a number and press enter')}else{setIsRunning(!isRunning)}}
+            }></i>}
+          { <i className={`fa-solid w-10 fa-stop fs-2 m-3 ${isRunning || timer > 0 || countdown ? "text-primary cursor-pointerx" : "text-secondary"}`} onClick={() => {reset()}}></i>}
         </div>
       </div>
 
@@ -70,26 +74,29 @@ const Home = () => {
       </div>
       <div className="row my-3">
         <div className="col-6 d-flex flex-column mx-auto align-items-center">
-          <input
-            type="number"
-            className="form-control text-center"
-            id="numberToStartCounter"
-            placeholder="Enter a number for counting start and press [Enter] key..."
-            value={startTimerValue}
-            disabled={isRunning}
-            onChange={(e) => { setStartTimerValue(e.target.value) }}
-            onKeyUp={(e) => { if(e.key === "Enter") {setTimer(Number(startTimerValue))} }}
-          />
+          {!reachedZero 
+            ?
+            <input
+              type="number"
+              className="form-control text-center"
+              id="numberToStartCounter"
+              placeholder="Enter a number for counting start and press [Enter] key..."
+              value={startTimerValue}
+              disabled={isRunning}
+              onChange={(e) => { setStartTimerValue(e.target.value) }}
+              onKeyUp={(e) => { if(e.key === "Enter") {setTimer(Number(startTimerValue))} }}
+            />
+            :
+            <div className="col-6">
+              <div className="alert alert-primary d-flex align-items-center" role="alert">
+                <i className="bi bi-info-lg flex-shrink-0 me-2"></i>
+                <div>0 time has been reached! Press stop button to reset timer.</div>
+              </div>
+            </div>
+          }
         </div>
       </div>
-      <div className={`row my-3 ${reachedZero? "" : "invisible"}`}>
-        <div className="col-6">
-        <div className="alert alert-primary d-flex align-items-center" role="alert">
-        <i className="bi bi-info-lg flex-shrink-0 me-2"></i>
-        <div>0 time has been reached! Press stop button to reset timer.</div>
-        </div>
-        </div>
-      </div>
+      
     </div>
   );
 };
